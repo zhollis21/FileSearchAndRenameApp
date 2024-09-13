88a1aaf778cc764fdaa65af6575f4e03b9b6e2f9
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2024-09-13 14:38:01 -0400

    Minor fixes (#8)
    * Remove testing logs
    
    * Adding github actions files to the solutions folder
    
    * Testing adding unit tests
    
    * Removing broken project
    
    * Adding a UnitTest
    
    * Fixing unit test
    
    * Passes now
    
    * It now fails
    
    * Small naming refactor
    
    * Update Tests/FileSearchAndRenameAppTests/UnitTest1.cs

---
+++ Tests/FileSearchAndRenameAppTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace FileSearchAndRenameAppTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
