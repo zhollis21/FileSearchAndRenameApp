22d1ec10830e9615fc8e34fcff4ba48e3b6dd198
zhollis21 <zhollis21@gmail.com>
2024-09-13 13:44:15 -0400

    Adding a UnitTest

---
+++ Tests/FileSearchAndRenameAppTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace FileSearchAndRenameAppTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
