23d9c83d2fc9338ff367634852391429f4704b4a
zhollis21 <zhollis21@gmail.com>
2024-09-13 13:26:38 -0400

    Testing adding unit tests

---
+++ FileSearchAndRenameAppTests/ProgramTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit;
+
+namespace FileSearchAndRenameAppTests;
+
+[TestFixture]
+public class ProgramTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual(1, 2);
+    }
+}
